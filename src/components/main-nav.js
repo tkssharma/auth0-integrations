63ad9ce1f8778de921aadd311b105908ed880068
tkssharma <tarun.softengg@gmail.com>
2024-10-24 14:30:33 +0530

    init

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -1,4 +1,4 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import React from "react";
 
 const MainNav = () => (
@@ -25,7 +25,7 @@ const MainNav = () => (
       className="nav-link"
       activeClassName="router-link-exact-active"
     >
-      External API
+      API
     </NavLink>
   </div>
 );
